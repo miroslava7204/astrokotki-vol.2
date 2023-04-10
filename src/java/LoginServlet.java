a5062b7538ad45129a92e640fe2c038167809044
Miroslava Lazarova <miroslava.m.lazarova@gmail.com>
2023-04-10 14:37:55 +0300

    login/register

---
+++ src/java/LoginServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        if (user.equals("Hristov")
+                && pass.equals("1234")) {
+            response.getWriter().println("Login successful");
+        } else {
+            response.getWriter().println("Login failed");
+        }
+    }
+}
